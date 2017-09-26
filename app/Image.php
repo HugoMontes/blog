b03bacfd9f22f15769a7cfd951a193bcda74b19a
Victor Hugo <victorzevallos@protonmail.com>
2017-09-26 14:22:29 -0500

    Modelos

--- app/Image.php
+++ app/Image.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Image extends Model
 {
     protected $fillable = ['name', 'article_id'];
+
+    public function article()
+    {
+        return $this->belongsTo('App\Article');
+    }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     protected $fillable = ['name'];
+
+    public function articles()
+    {
+        return $this->belongsToMany('App\Article')->withTimestamps();
+    }
 }
